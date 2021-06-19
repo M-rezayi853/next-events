0483c9c1698e53314d85c28c78fbbcbbeaea21cb
Mahdi Rezayi <mahdi.rezzai93@gmail.com>
2021-06-19 15:35:32 +0430

    nextEvent, routing frontend project

---
+++ components/events/EventList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import EventItem from './EventItem'
+import classes from './EventList.module.css'
+
+function EventList({ items }) {
+  return (
+    <ul className={classes.list}>
+      {items.map(item => (
+        <EventItem key={item.id} {...item} />
+      ))}
+    </ul>
+  )
+}
+
+export default EventList
